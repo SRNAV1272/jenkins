5aed30efc67d6c62b7f93ce3567e10a55ae9110b
K Sai Rajesh <sairajesh.korla@navajna.com>
2024-01-29 14:56:06 +0530

    jenkins commit 2 remote push 29-01-2024

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world !")
+import cors from 'cors'
+import express from 'express'
+
+const app = new express()
+
+app.use(cors())
+const port = 8080
+
+app.listen(port, () => {
+    console.log(`Server is listening at port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send({
+        health: 'good !'
+    })
+})
